afb65300f3d5c1b352c6b76a622b097a4359d453
Shmew <Shmeww@gmail.com>
2019-10-15 18:53:37 -0500

    Update ScatterChart.fs

--- docs/Examples/ScatterChart.fs
+++ docs/Examples/ScatterChart.fs
@@ -5,28 +5,25 @@ open Feliz
 open Feliz.Plotly
 open Fable.Core.Experimental
 
-let test = 
-    plot.data [
-        data.scatter [
-            data.scatter.x [ 1; 2; 3; 4]
-            data.scatter.y [ 10; 15; 13; 17 ]
-            data.scatter.mode [ "markers" ]
-        ]
-        data.scatter [
-            data.scatter.x [ 2; 3; 4; 5 ]
-            data.scatter.y [ 16; 5; 11; 9 ]
-            data.scatter.mode [ "lines" ]
-        ]
-        data.scatter [
-            data.scatter.x [ 1; 2; 3; 4 ]
-            data.scatter.y [ 12; 9; 15; 12 ]
-            data.scatter.mode [ "lines+markers" ]
-        ]
-    ]
-
 let chart () =
     Plotly.plot [
-        test
+        plot.data [
+            data.scatter [
+                data.scatter.x [ 1; 2; 3; 4]
+                data.scatter.y [ 10; 15; 13; 17 ]
+                data.scatter.mode [ "markers" ]
+            ]
+            data.scatter [
+                data.scatter.x [ 2; 3; 4; 5 ]
+                data.scatter.y [ 16; 5; 11; 9 ]
+                data.scatter.mode [ "lines" ]
+            ]
+            data.scatter [
+                data.scatter.x [ 1; 2; 3; 4 ]
+                data.scatter.y [ 12; 9; 15; 12 ]
+                data.scatter.mode [ "lines+markers" ]
+            ]
+        ]
         plot.layout [
             layout.plot_bgcolor colors.aqua
         ]
@@ -35,5 +32,4 @@ let chart () =
         ]
         plot.debug true
         plot.className "wowie zowie"
-        plot.onInitialized(fun _ plotHtml -> Fable.Core.JS.console.log plotHtml)
     ]
