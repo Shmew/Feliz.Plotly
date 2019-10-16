1a2647d227ba1a0a9073b4a14cd5a4d733ab795c
Shmew <Shmeww@gmail.com>
2019-10-16 12:37:05 -0500

    Docs

--- src/Feliz.Generator.Plotly/Program.fs
+++ src/Feliz.Generator.Plotly/Program.fs
@@ -6,25 +6,22 @@ module Program =
 
     [<EntryPoint>]
     let main _ =
-        async {
-            let api = ApiParser.parseApi()
-            let plotlyFile = __SOURCE_DIRECTORY__ @@ "../Feliz.Plotly/Plotly.fs"
-            let interopFile = __SOURCE_DIRECTORY__ @@ "../Feliz.Plotly/Interop.fs"
-            let typesFile = __SOURCE_DIRECTORY__ @@ "../Feliz.Plotly/Types.fs"
-            let propsDir = __SOURCE_DIRECTORY__ @@ "../Feliz.Plotly/Props"
+        let api = ApiParser.parseApi()
+        let plotlyFile = __SOURCE_DIRECTORY__ @@ "../Feliz.Plotly/Plotly.fs"
+        let interopFile = __SOURCE_DIRECTORY__ @@ "../Feliz.Plotly/Interop.fs"
+        let typesFile = __SOURCE_DIRECTORY__ @@ "../Feliz.Plotly/Types.fs"
+        let propsDir = __SOURCE_DIRECTORY__ @@ "../Feliz.Plotly/Props"
 
-            Render.componentDocument api.GeneratorComponentApi |> File.writeString false plotlyFile
+        Render.componentDocument api.GeneratorComponentApi |> File.writeString false plotlyFile
 
-            Render.interopDocument api.GeneratorComponentApi |> File.writeString false interopFile
+        Render.interopDocument api.GeneratorComponentApi |> File.writeString false interopFile
 
-            Render.typesDocument api.GeneratorComponentApi |> File.writeString false typesFile
+        Render.typesDocument api.GeneratorComponentApi |> File.writeString false typesFile
 
-            api.GeneratorComponentApi
-            |> Render.propsDocument true
-            |> List.iter (fun (name, doc) ->
-                let path = propsDir @@ (sprintf "%s.fs" (name |> String.upperFirst))
-                File.writeString false path doc)
+        api.GeneratorComponentApi
+        |> Render.propsDocument true
+        |> List.iter (fun (name, doc) ->
+            let path = propsDir @@ (sprintf "%s.fs" (name |> String.upperFirst))
+            File.writeString false path doc)
 
-            return 0
-        }
-        |> Async.RunSynchronously
+        0
