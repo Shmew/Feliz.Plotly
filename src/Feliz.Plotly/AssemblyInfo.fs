8e23c061a22feeba7d123e100700af64cdd0ec19
Shmew <Shmeww@gmail.com>
2019-11-16 17:27:55 -0600

    Bump version to 0.17.2

--- src/Feliz.Plotly/AssemblyInfo.fs
+++ src/Feliz.Plotly/AssemblyInfo.fs
@@ -6,8 +6,8 @@ open System.Runtime.CompilerServices
 [<assembly: AssemblyTitleAttribute("Feliz.Plotly")>]
 [<assembly: AssemblyProductAttribute("Feliz.Plotly")>]
 [<assembly: AssemblyDescriptionAttribute("Fable bindings written in the Feliz-style for plotly.js")>]
-[<assembly: AssemblyVersionAttribute("0.17.1")>]
-[<assembly: AssemblyFileVersionAttribute("0.17.1")>]
+[<assembly: AssemblyVersionAttribute("0.17.2")>]
+[<assembly: AssemblyFileVersionAttribute("0.17.2")>]
 [<assembly: AssemblyConfigurationAttribute("Release")>]
 [<assembly: InternalsVisibleToAttribute("Feliz.Plotly.Tests")>]
 do ()
@@ -16,7 +16,7 @@ module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Feliz.Plotly"
     let [<Literal>] AssemblyProduct = "Feliz.Plotly"
     let [<Literal>] AssemblyDescription = "Fable bindings written in the Feliz-style for plotly.js"
-    let [<Literal>] AssemblyVersion = "0.17.1"
-    let [<Literal>] AssemblyFileVersion = "0.17.1"
+    let [<Literal>] AssemblyVersion = "0.17.2"
+    let [<Literal>] AssemblyFileVersion = "0.17.2"
     let [<Literal>] AssemblyConfiguration = "Release"
     let [<Literal>] InternalsVisibleTo = "Feliz.Plotly.Tests"
